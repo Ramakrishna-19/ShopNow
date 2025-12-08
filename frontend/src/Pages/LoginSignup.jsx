dab2df092e120b7a719f9f744bc98a4aba34de78
Ramakrishna-19 <ramakrishnadyavari1920@gmail.com>
2025-12-09 03:15:14 +0530

    Adding backend user cart

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,18 +1,96 @@
 import React from 'react'
 import './CSS/loginsignup.css'
 
+
 const LoginSignup = () => {
+
+    const [state, setState] = React.useState("Sign Up");
+    const [message, setMessage] = React.useState("");
+    const [formData, setFormData] = React.useState({
+      username: "",
+      password: "",
+      email: ""
+    })
+
+    const changeHandler = (e)=>{
+      setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+      })
+    }
+
+    const login = async()=>{
+      console.log("Login Function Executed", formData);
+      let responseData;
+
+      await fetch('http://localhost:4000/login', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/form-data',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: formData.email,
+          password: formData.password
+        })
+      })
+      .then((response)=>response.json())
+      .then((data)=>responseData=data);
+
+      if(responseData.success){
+        setMessage("Login successful");
+
+        setTimeout(() => {
+          localStorage.setItem('auth-token', responseData.token);
+          window.location.replace("/");
+        }, 1000);
+      }
+      else{
+        setMessage("Can not find the user");
+
+        setTimeout(() => {
+          setMessage("");
+        }, 2000);
+      }
+    }
+
+
+    const signup = async()=>{
+        console.log("Signup Function Executed", formData);
+        let responseData;
+        await fetch('http://localhost:4000/signup', {
+          method: 'POST',
+          headers: {
+            Accept: 'application/form-data',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        }).then((response)=>response.json()).then((data)=>responseData=data)
+
+        if(responseData.success){
+          localStorage.setItem('auth-token', responseData.token);
+          window.location.replace("/");
+        }
+        else{
+          alert(responseData.error);
+        }
+    }
+
   return (
     <div className='loginsignup'>
         <div className="loginsignup-container">
-            <h1>Sign Up</h1>
+            <h1>{state}</h1>
             <div className="loginsignup-fields">
-                <input type='text' placeholder='Your Name'/>
-                <input type='email' placeholder='Email Address'/>
-                <input type='password' placeholder='Password'/>
+                {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type='text' placeholder='Your Name'/> : <></>}
+                <input name='email' value={formData.email} onChange={changeHandler} type='email' placeholder='Email Address'/>
+                <input name='password' value={formData.password} onChange={changeHandler} type='password' placeholder='Password'/>
             </div>
-            <button>Continue</button>
-            <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+            {message && <p style={{textAlign:"center", color:"green"}}>{message}</p>}
+            <button onClick={()=>{state==="Login"?login():signup()}}>Continue</button>
+            {state==="Sign Up"?
+            <p className="loginsignup-login">Already have an account? <span onClick={()=> {setState("Login")}} style={{cursor: "pointer"}}>Login here</span></p> : <></>}
+            {state==="Login"?
+            <p className="loginsignup-login">Create an account? <span onClick={()=> {setState("Sign Up")}} style={{cursor: "pointer"}}>Click here</span></p> : <></>}
             <div className="loginsignup-agree">
                 <input type="checkbox" name='' id='' />
                 <p>By continuing, i agree to the terms and policies</p>
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,6 +1,30 @@
-import React, {useContext} from 'react'
-import {useParams} from 'react-router-dom'
-import all_product from '../Components/Assets/all_product'
+// import React, {useContext} from 'react'
+// import {useParams} from 'react-router-dom'
+// import all_product from '../Components/Assets/all_product'
+// import { ShopContext } from '../Context/ShopContext'
+// import Breadcrums from '../Components/Breadcrums/Breadcrums';
+// import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+// import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+// import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
+
+// const Product = () => {
+//     const {all_product} = useContext(ShopContext);
+//     const {productId } = useParams();
+//     const product = all_product.find((e) => e.id === Number(productId));
+//     return (
+//         <div>
+//             <Breadcrums product={product}/>
+//             <ProductDisplay product={product}/>
+//             <DescriptionBox/>
+//             <RelatedProducts/>
+//         </div>
+//     )
+// }
+
+// export default Product
+
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
 import { ShopContext } from '../Context/ShopContext'
 import Breadcrums from '../Components/Breadcrums/Breadcrums';
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
@@ -8,17 +32,28 @@ import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
 import RelatedProducts from '../Components/RelatedProducts/RelatedProducts';
 
 const Product = () => {
-    const {all_product} = useContext(ShopContext);
-    const {productId } = useParams();
+    const { all_product } = useContext(ShopContext);
+    const { productId } = useParams();
+    
     const product = all_product.find((e) => e.id === Number(productId));
+
+    if (!product) {
+        return (
+            <div style={{ padding: "40px", fontSize: "20px" }}>
+                Loading product...
+            </div>
+        );
+    }
+
     return (
         <div>
-            <Breadcrums product={product}/>
-            <ProductDisplay product={product}/>
-            <DescriptionBox/>
-            <RelatedProducts/>
+            <Breadcrums product={product} />
+            <ProductDisplay product={product} />
+            <DescriptionBox />
+            <RelatedProducts />
         </div>
-    )
+    );
 }
 
-export default Product
+export default Product;
+
